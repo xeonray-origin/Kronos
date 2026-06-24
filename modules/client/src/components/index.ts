a650ec2b7d6d8805c57f64c3bdd06d655d7cdf99
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2026-06-24 21:31:46 +0530

    chore(ui): named exports for components from the folder root

--- modules/client/src/components/index.ts
+++ modules/client/src/components/index.ts
@@ -1,7 +1,7 @@
 export { Task } from './task';
-export { CreateTaskModal } from './create-task-modal/create-task-modal';
-export { LoginForm } from './login-form/login-form';
-export { SignupForm } from './signup-form/signup-form';
+export { CreateTaskModal } from './create-task-modal';
+export { LoginForm } from './login-form';
+export { SignupForm } from './signup-form';
 export { Sidebar } from './sidebar';
 export { Topbar } from './topbar';
 export { Timer } from './timer';
